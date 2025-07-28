643f4f06abe8a03a0a4350ff9c78bec1c0dd90a7
Tide-sunrise <1834049428@qq.com>
2025-07-28 23:50:17 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <HelloWorld msg="欢迎，这是张栩嘉的主页"/>-->
+  <Homepage/>.
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from "@/views/homepage.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Homepage
   }
 }
 </script>
@@ -21,6 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  //margin-top: 60px;
 }
 </style>
